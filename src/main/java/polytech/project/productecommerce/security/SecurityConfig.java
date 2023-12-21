53783861d2bc31b1e8cd93978ea46b049da79c45
jihed0405 <jihed0405@gmail.com>
2023-12-21 17:32:21 +0100

    change security file to spring 6

--- src/main/java/polytech/project/productecommerce/security/SecurityConfig.java
+++ src/main/java/polytech/project/productecommerce/security/SecurityConfig.java
@@ -1,63 +1,67 @@
 package polytech.project.productecommerce.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import polytech.project.productecommerce.model.Role;
 import polytech.project.productecommerce.security.jwt.JwtAuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource());
+        http.csrf((csrf) -> csrf.disable());
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/authentication/**").permitAll());
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/authentication/**").permitAll());
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/product").permitAll());
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/category").permitAll());
+        http.authorizeHttpRequests(
+                authorize -> authorize.requestMatchers("/api/product/**").hasRole(Role.ADMIN.name()));
+        http.authorizeHttpRequests(
+                authorize -> authorize.requestMatchers("/api/supplier/**").hasRole(Role.ADMIN.name()));
+        http.authorizeHttpRequests(
+                authorize -> authorize.requestMatchers("/api/category/**").hasRole(Role.ADMIN.name()));
+        http.authorizeHttpRequests(
+                authorize -> authorize.requestMatchers("/api/customer/**").hasRole(Role.ADMIN.name()));
+
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests()
-                .antMatchers("/api/authentication/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/product").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/category").permitAll()
-                .antMatchers("/api/product/**").hasRole(Role.ADMIN.name())
-                .antMatchers("/api/supplier/**").hasRole(Role.ADMIN.name())
-                .antMatchers("/api/category/**").hasRole(Role.ADMIN.name())
-                .antMatchers("/api/customer/**").hasRole(Role.ADMIN.name())
-                .anyRequest().authenticated();
-        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
-    }
-
-    @Override
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 
     @Bean
@@ -75,5 +79,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .allowedMethods("*");
             }
         };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
